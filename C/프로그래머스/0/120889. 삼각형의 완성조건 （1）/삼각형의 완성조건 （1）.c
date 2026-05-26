39dbd2ee9ed6d5499fd4db6b2ca7c5d7f2684d5a
Jiyun_Han <162584369+J-HanRyang@users.noreply.github.com>
2026-05-26 16:42:59 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ C/프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+    return *(int*)a - *(int*)b;
+}
+
+// sides_len은 배열 sides의 길이입니다.
+int solution(int sides[], size_t sides_len) {
+    int answer = 0;
+    
+    qsort(sides, sides_len, sizeof(int), compare);
+    if (sides[0] + sides[1] > sides[2]) answer = 1;
+    else answer = 2;
+    
+    return answer;
+}
