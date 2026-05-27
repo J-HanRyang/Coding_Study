8f33097e91bbe32e5907d90d49655bf2b4994e2d
Jiyun_Han <162584369+J-HanRyang@users.noreply.github.com>
2026-05-27 13:11:29 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ C/프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+    return *(int*)a - *(int*)b;
+}
+
+// numbers_len은 배열 numbers의 길이입니다.
+int solution(int numbers[], size_t numbers_len) {
+    int answer = 0;
+    
+    qsort(numbers, numbers_len, sizeof(int), compare);
+    
+    if ((numbers[0]*numbers[1]) > (numbers[numbers_len-1]*numbers[numbers_len-2])) answer = numbers[0]*numbers[1];
+    else answer = numbers[numbers_len-1]*numbers[numbers_len-2];
+    
+    return answer;
+}
